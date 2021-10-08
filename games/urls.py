08c1cdfe086290c18bb985c07299fe46544a4109
Luca Alexandru <luca_github@outlook.com>
2021-10-08 17:11:17 +0200

    URL fix

--- games/urls.py
+++ games/urls.py
@@ -5,6 +5,6 @@ app_name = "games"
 
 urlpatterns = [
     path('all/', views.games_data, name='all_games'),
-    path('add_book/', views.add_game, name='add_game'),
-    path('delete/<int:book_id>/', views.game_delete, name='game_delete')
+    path('add_game/', views.add_game, name='add_game'),
+    path('delete/<int:game_id>/', views.game_delete, name='game_delete')
 ]
