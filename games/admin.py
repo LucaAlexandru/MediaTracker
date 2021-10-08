301e41f47513f0c2e42d6e0f79fe4bb52d89b637
Luca Alexandru <luca_github@outlook.com>
2021-10-08 15:22:44 +0200

    update to games

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from games.models import Game
 
 # Register your models here.
+
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ("game_name", "game_developer", "game_platform", "game_release_date",
+                    "game_genre", "game_rating", "game_status")
+
+
+admin.site.register(Game, GameAdmin)
