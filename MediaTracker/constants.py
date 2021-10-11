dad01aa41303407baf219ceb0a81e0a735541c3f
Luca Alexandru <luca_github@outlook.com>
2021-10-11 12:01:14 +0200

    models and constants for Films app

--- MediaTracker/constants.py
+++ MediaTracker/constants.py
@@ -21,6 +21,17 @@ RATING = (
      ("unrated", "Unrated"), ("1", "1"), ("2", "2"), ("3", "3"), ("4", "4"), ("5", "5")
 )
 
+# FILMS
+RATING_TEN = (
+     ("unrated", "Unrated"), ("1", "1"), ("2", "2"), ("3", "3"), ("4", "4"), ("5", "5"),
+     ("6", "6"), ("7", "7"), ("8", "8"), ("9", "9"), ("10", "10")
+)
+
+FILM_TYPE = (
+    ("movie", "Movie"), ("tv_show", "TV Show"), ("animated_film", "Animated film"),
+    ("animated_show", "Animated show")
+)
+
 # GAMES
 
 GAME_STATUS_CHOICES = (
